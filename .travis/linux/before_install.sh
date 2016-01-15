231ebb24394b8166d8776ba38ed1615fbb3117e7
Roman Proskuryakov <humbug@deeptown.org>
2016-01-15 04:51:08 +0300

    add: find Sodium 	add: link Sodium into toxcore

--- .travis/linux/before_install.sh
+++ .travis/linux/before_install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 pip install --user cpp-coveralls
+
+DEPS="$HOME/deps/"
+export DEPS_PREFIX_PATH="$DEPS/usr"
+mkdir "$DEPS"
+mkdir "DEPS_PREFIX_PATH"
+
+wget https://github.com/jedisct1/libsodium/archive/1.0.0.tar.gz -O $DEPS/libsodium-1.0.0.tar.gz
+tar xzvf  $DEPS/libsodium-1.0.0.tar.gz  -C $DEPS
+(cd $DEPS/libsodium-1.0.0/; ./autogen.sh; ./configure --prefix=${DEPS_PREFIX_PATH})
+make -j4 -C $DEPS/libsodium-1.0.0
+make install -C $DEPS/libsodium-1.0.0
